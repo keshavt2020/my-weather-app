b7887aa868083772c77f8c79273c43bdb6a408c3
KeshavXPS15 <KeshavXPS15@local.com>
2020-01-30 17:23:55 +0000

    Implemented Geocode and Forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,1 +1,19 @@
-console.log('forecast')
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    //const url = 'https://api.darksky.net/forecast/9dbdce9c526100df3dd14e1af614cf32/' + latitude + ',' + longitude
+    const url = 'https://api.darksky.net/forecast/9dbdce9c526100df3dd14e1af614cf32/' + latitude + ',' + longitude + '?units=si'
+    //console.log(url)
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+        }
+    })
+}
+
+module.exports = forecast
