bf5d84c448bd0048ecfd7e46bb66d56fbac91c05
KeshavXPS15 <KeshavXPS15@local.com>
2020-01-30 12:49:24 +0000

    Init commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+
+const app = express()
+
+app.get('/',  (req, res) => {
+    res.send('Hello Weather')
+  })
+
+
+  app.get('*',  (req, res) => {
+    res.send('Page Not Found')
+  })
+
+
+  app.listen(3000, () => {
+    console.log('Express Server Started')
+})
